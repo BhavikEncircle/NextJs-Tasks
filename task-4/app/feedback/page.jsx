5e84674e6f6341f9d53186a7a5c0feeb75740582
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-01-23 16:44:43 +0530

    refactor : renamed page.js to page.jsx

--- task-4/app/feedback/page.jsx
+++ task-4/app/feedback/page.jsx
@@ -1,22 +1,16 @@
-export default async function FeedbackPage() {
-  const feedback = await getFeedback()
+import FeedbackForm from '../components/FeedbackForm'
+import FeedbackList from '../components/FeedbackList'
+
+export default function FeedbackPage() {
   return (
-    <div>
-      <form action={submitFeedback}>
-        <input type='text' name='name' placeholder='Enter name' />
-        <input type='text' name='email' placeholder='Enter email' />
-        <input type='text' name='message' placeholder='Enter message' />
-        <button type='submit'>Submit</button>
-      </form>
-      <ul>
-        {feedback.map((item) => (
-          <li key={item.id}>
-            <h1>Name : {item.name}</h1>
-            <h5>Email : {item.email}</h5>
-            <p>Message : {item.message}</p>
-          </li>
-        ))}
-      </ul>
+    <div className='max-w-3xl mx-auto p-6'>
+      <h1 className='text-3xl font-bold mb-6 text-center'>Feedback</h1>
+
+      <FeedbackForm />
+
+      <div className='mt-10'>
+        <FeedbackList />
+      </div>
     </div>
   )
 }
