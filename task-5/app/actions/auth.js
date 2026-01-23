59a54840f825bd331d8a000e23265afce3dab84a
BhavikEncircle <bhavikp.encircle@gmail.com>
2026-01-23 13:41:23 +0530

    Initiated task-

---
+++ task-5/app/actions/auth.js
@@ -0,0 +1,24 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export async function login(formData) {
+  const username = formData.get("username");
+  const password = formData.get("password");
+
+  if (username !== "admin" || password !== "123456") {
+    throw new Error("Invalid credentials");
+  }
+
+  const cookieStore = await cookies();
+  cookieStore.set("user", JSON.stringify({ username }), { httpOnly: true });
+
+  redirect("/dashboard");
+}
+export async function logout() {
+  "use server";
+  const cookieStore = await cookies();
+  cookieStore.delete("user");
+  redirect("/login");
+}
